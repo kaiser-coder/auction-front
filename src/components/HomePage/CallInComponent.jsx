e43135f8e71c81d98caae58c362604bca7b2737b
kaiser-coder <miarintsoajohary@yahoo.com>
2021-12-11 16:17:16 +0100

    fea(page): build Homepage

---
+++ src/components/HomePage/CallInComponent.jsx
@@ -0,0 +1,15 @@
+export default function CallInComponent() {
+   return (
+      <section class="call-in-section padding-top pt-max-xl-0">
+        <div class="container">
+            <div class="call-wrapper cl-white bg_img" data-background="/assets/images/call-in/call-bg.png">
+                <div class="section-header">
+                    <h3 class="title">Register for Free & Start Bidding Now!</h3>
+                    <p>From cars to diamonds to iPhones, we have it all.</p>
+                </div>
+                <a href="sign-up.html" class="custom-button white">Register</a>
+            </div>
+        </div>
+    </section>
+   )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import BrowseHighlight from "../components/HomePage/BrowseHighlights"
+import BrowseHighlight from '../components/HomePage/BrowseHighlights'
+import ProductsList from '../components/HomePage/ProductsList'
 
 // Constants
 const browseItems = [
@@ -26,12 +27,76 @@ const browseItems = [
       img: '/assets/images/auction/06.png',
       name: 'Real Estate'
    },
-];
+]
+
+const products = {
+   jewelries: {
+      category: 'Jewelry',
+      description: 'Online jewelry auctions where you can bid now and save money',
+      items: [
+         {  
+            img: 'assets/images/auction/jewelry/auction-1.jpg',
+            title: 'Gold ring with clear stone',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+         {  
+            img: 'assets/images/auction/jewelry/auction-2.jpg',
+            title: 'Gold ring with clear stone',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+         {  
+            img: 'assets/images/auction/jewelry/auction-3.jpg',
+            title: 'Gold ring with clear stone',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+      ]
+   },
+   watches: {
+      category: 'Watches',
+      description: 'Shop for men & women designer brand watches',
+      items: [
+         {  
+            img: 'assets/images/auction/watches/auction-1.jpg',
+            title: 'Vintage rolex',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+         {  
+            img: 'assets/images/auction/watches/auction-2.jpg',
+            title: 'Lady\'s Vintage Rolex Datejust',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+         {  
+            img: 'assets/images/auction/watches/auction-3.jpg',
+            title: 'Lady\'s retro diamond',
+            currentBid: '876.00',
+            buyNow: '5,00.00',
+            bids: '30',
+            time: ''
+         },
+      ]
+   }
+}
 
 export default function HomePage() {
    return (
       <>
          <BrowseHighlight items={browseItems}/>
+         <ProductsList items={products}/>
       </>
    )
 }
